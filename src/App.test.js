ceeb29a1a7e8d4d50633c8e3f66e8d116693356e
Roy <technicmk@gmail.com>
2017-12-06 22:11:13 +0200

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+	const div = document.createElement('div')
+	ReactDOM.render(<App />, div)
+})
+
+it('does some weird stuff', () => {
+	expect(5).toEqual(5)
+})
